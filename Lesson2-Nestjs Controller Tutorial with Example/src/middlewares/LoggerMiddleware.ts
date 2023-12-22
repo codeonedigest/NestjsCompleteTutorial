4017afff097a43e66379f6461475b8adb041dbd5
codeonedigest <codeonedigest@gmail.com>
2023-12-22 15:43:28 +0530

    updated the middle ware class to have function and class code

--- Lesson2-Nestjs Controller Tutorial with Example/src/middlewares/LoggerMiddleware.ts
+++ Lesson2-Nestjs Controller Tutorial with Example/src/middlewares/LoggerMiddleware.ts
@@ -2,6 +2,17 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { Logger } from '@nestjs/common';
 
+/*@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  logger = new Logger(LoggerMiddleware.name);
+
+  use(req: Request, res: Response, next: NextFunction) {
+    this.logger.debug('Request...' + req   );
+    next();
+  } 
+}*/
+
+
 export function loggerFunction(req: Request, res: Response, next: NextFunction) {
     console.log(`Request...` + req);
     next();
