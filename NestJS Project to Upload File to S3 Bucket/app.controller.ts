5c769cb194b2ff632c237ab95afbcaccafdf13c6
codeonedigest <codeonedigest@gmail.com>
2024-10-27 06:55:02 +0530

    Adding service and controller for download and list objects

--- NestJS Project to Upload File to S3 Bucket/app.controller.ts
+++ NestJS Project to Upload File to S3 Bucket/app.controller.ts
@@ -4,6 +4,7 @@ import {
   UploadedFile,
   UseInterceptors,
   Logger,
+  Get,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
@@ -17,8 +18,25 @@ export class AppController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    
     this.logger.log("Received request to upload file " + file);
     return this.appService.uploadFile(file);
+  }
+
+
+  @Get('list')
+  async listObjects(): Promise<any> {
+    this.logger.log("Listing content of bucket ");
+    let responseData = await this.appService.listObject();
+    this.logger.log('Response Data ' + responseData);
+    return responseData;
+  }
+
+
+  @Get('download')
+  async getObjects(): Promise<any> {
+    this.logger.log("Downloading file from s3 bucket ");
+    let responseData = await this.appService.downloadFile();
+    this.logger.log('Response Data ' + responseData);
+    return responseData;
   }
 }
--- NestJS Project to Upload File to S3 Bucket/app.service.ts
+++ NestJS Project to Upload File to S3 Bucket/app.service.ts
@@ -1,15 +1,21 @@
 import { Injectable, Req, Res, Logger } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
+import { AnyArn } from 'aws-sdk/clients/groundstation';
+
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
 
-  AWS_S3_BUCKET = 'sea-ecomm-asia-dev';
+  AWS_S3_BUCKET = '<bucket name>';
+
   s3 = new AWS.S3({
-    accessKeyId: '<keyID>',
-    secretAccessKey: '<Secret>',
+    region: 'ap-southeast-1',
+    accessKeyId: '<IAM user key ID>',
+    secretAccessKey: '<IAM user secret key>',
   });
+
+
 
   async uploadFile(file) {
     this.logger.log('uploading file ' + file);
@@ -24,7 +30,8 @@ export class AppService {
   }
 
   async s3_upload(file, bucket, name, mimetype) {
-    const targetLocation = 'POC/Australia/'+String(name);
+
+    const targetLocation = 'POC/'+String(name);
     const params = {
       Bucket: bucket,
       Key: targetLocation,
@@ -33,7 +40,7 @@ export class AppService {
       ContentDisposition: 'inline',
       CreateBucketConfiguration: {
         LocationConstraint: 'ap-south-1',
-      },
+        },
     };
 
     try {
@@ -44,4 +51,42 @@ export class AppService {
       console.log(e);
     }
   }
+
+
+  async listObject(): Promise<any> {
+    this.logger.log('Listing objects in S3 bucket');
+  
+    var params = { 
+      Bucket: this.AWS_S3_BUCKET,
+      Delimiter: '/',
+      Prefix: 'POC/'
+     }
+   
+    let result = await this.s3.listObjectsV2(params).promise();
+      
+    console.log("List object: " + result )
+    return result;  
+
+  }
+
+
+
+  async downloadFile() {
+    this.logger.log('Downloading file from S3 bucket ');
+  
+    var params = { 
+      Bucket: this.AWS_S3_BUCKET,
+      Key: 'POC/flower-jasmin.jpg',
+      Expires: 30
+     }
+
+     const url = await this.s3.getSignedUrl('getObject', params);
+
+    console.log("Url to download file: " + url);
+    return url;
+  }
+
+
+ 
+
 }
